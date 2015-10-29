ccedc9b2ba78ad460c95f8d92d9b6465abdaef61
Victor Quinn <mail@victorquinn.com>
2015-10-29 16:46:49 -0400

    Initial commit

---
+++ CategoriesSampleApp/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  CategoriesSampleApp
+//
+//  Created by Victor Quinn on 10/29/15.
+//  Copyright © 2015 LocationKit. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
